9b24abb1cbc58e888e8e852935a960e7639e3b12
Modeus <ivanigolkin05@gmail.com>
2026-02-28 00:49:54 +0300

    sdfsdgg

--- something.py
+++ something.py
@@ -1,3 +1,12 @@
 print('Это файл прямиком с GitHub')
 
 print('АААААвыаываыафы')
+
+def f(a: int|float, s: str) -> str:
+    return a * s
+
+def f2(lst: list[int|float], tpl: tuple[str, list], sep: str='') -> str:
+    return 'ds'
+
+def f3(dct: dict[int, str|float]) -> dict:
+    return {3: 'd'}
